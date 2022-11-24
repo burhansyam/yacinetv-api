9f4453b8ecbf7d4733a150cc0eccfab7f44bc765
Burhan Syam <23581412+burhansyam@users.noreply.github.com>
2022-11-24 10:20:21 +0700

    Update routes.py

--- routes.py
+++ routes.py
@@ -9,6 +9,10 @@ router = APIRouter(tags=["YacineTV"])
 def categories():
     return ytv.get_categories()
 
+@router.get("/event")
+def event():
+    return ytv.get_event()
+
 @router.get("/categories/{category_id}/channels")
 def channels(category_id: int):
     return ytv.get_category_channels(category_id)
@@ -16,3 +20,7 @@ def channels(category_id: int):
 @router.get("/channel/{channel_id}")
 def channel(channel_id: int):
     return ytv.get_channel(channel_id)
+
+@router.get("/event/{channel_id}")
+def channel(event_id: int):
+    return ytv.get_events(event_id)
